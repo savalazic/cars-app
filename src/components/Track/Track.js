1685a6914b3cf13266d251cdc461c5b95f989409
savalazic <savalazic9@gmail.com>
2018-04-09 22:41:22 +0200

    Separate track grid

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,4 +1,5 @@
 // @flow
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
 
 import './Track.css';
@@ -7,9 +8,34 @@ type Props = {
   distance: number,
 };
 
+const NUM_PARTS = 10;
+
 const Track = ({ distance }: Props) => (
-  <div className="track" style={{ maxWidth: `calc(${distance}px * 100)` }}>
+  <div>
     <h2>{distance} km</h2>
+    <h3>1 part of track: {distance / NUM_PARTS} km</h3>
+    <div className="indicators">
+      {[...Array(NUM_PARTS)].map((e, i) => (
+        <div
+          className="indicator-item"
+          key={i}
+          style={{ width: `calc(100% / ${NUM_PARTS})` }}
+        >
+          {i + 1}xN
+        </div>
+      ))}
+    </div>
+    <div className="track">
+      <div className="track-row">
+        {[...Array(NUM_PARTS)].map((e, i) => (
+          <div
+            key={i}
+            className="track-column"
+            style={{ width: `calc(100% / ${NUM_PARTS})` }}
+          />
+        ))}
+      </div>
+    </div>
   </div>
 );
 
