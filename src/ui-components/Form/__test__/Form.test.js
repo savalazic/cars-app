89832c8cb98e6bbfe224f014ec0bc0dba7133e34
savalazic <savalazic9@gmail.com>
2018-04-11 00:55:00 +0200

    Remove leftovers

--- src/ui-components/Form/__test__/Form.test.js
+++ src/ui-components/Form/__test__/Form.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow, mount, render } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import Form from '../Form';
 
